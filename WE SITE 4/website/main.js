4f6fa7f2064c369fb8c57e491a93fb3db77170bf
MarkHarrison03 <96838167+MarkHarrison03@users.noreply.github.com>
2022-09-12 21:48:01 +0100

    Website Final

---
+++ WE SITE 4/website/main.js
@@ -0,0 +1,16 @@
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+})
+
+function toggleText() {
+    var text = document.getElementById("music-text");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
